a3cdaf10e413d874c4639f4923a1b66603077cbb
user <user@debian-thanh.insa-cvl.fr>
2017-01-11 00:25:55 +0100

    Java et Python

--- Java/Hierarchie/src/geometrie/Quadrilatere.java
+++ Java/Hierarchie/src/geometrie/Quadrilatere.java
@@ -1,6 +1,7 @@
 package geometrie;
 
-public class Quadrilatere {
+public class Quadrilatere extends Polygone {
+	int code;
 
 	/** 
 	 * @uml.property name="lesSegments"
@@ -37,6 +38,7 @@ public class Quadrilatere {
 		double perimetre = 0;
 		perimetre = lesSegments[0].longueur() + lesSegments[1].longueur()
 				+ lesSegments[2].longueur() + lesSegments[3].longueur();
+		System.out.println("Calcul du périmètre d'un quadrilatère");
 		return perimetre;
 	}
 
@@ -46,28 +48,50 @@ public class Quadrilatere {
 	public String toString(){
 		return lesSegments[0]+" "+lesSegments[1]+" "+lesSegments[2]+" "+lesSegments[3] ;	
 	}
+	
+	
+	/**
+	 */
+	public Quadrilatere(){
+		this(new Point(),new Point(),new Point(),new Point());
+		this.code = 1;
+		/*lesSegments[0] = new Segment(new Point(),new Point());
+					lesSegments[1] = new Segment(new Point(),new Point());
+					lesSegments[2] = new Segment(new Point(),new Point());
+					lesSegments[3] = new Segment(new Point(),new Point());*/
+	}
 
 
 	/**
 	 */
 	public Quadrilatere(Point p1, Point p2, Point p3, Point p4){
+		this.code = 1;
 		lesSegments[0] = new Segment(p1,p2);
 		lesSegments[1] = new Segment(p2,p3);
 		lesSegments[2] = new Segment(p3,p4);
 		lesSegments[3] = new Segment(p4,p1);
 		System.out.println("Quadrilatère");
 	}
-
-
-	/**
-	 */
-	public Quadrilatere(){
-		this(new Point(),new Point(),new Point(),new Point());
-		/*lesSegments[0] = new Segment(new Point(),new Point());
-					lesSegments[1] = new Segment(new Point(),new Point());
-					lesSegments[2] = new Segment(new Point(),new Point());
-					lesSegments[3] = new Segment(new Point(),new Point());*/
+	
+	
+	public Quadrilatere(Point p1, Point p2, Point p3, Point p4, String couleur, String texture)
+	{
+		this(p1, p2, p3, p4);
+		this.setCouleur(couleur);
+		this.setTexture(texture);
+		System.out.println("...avec couleur et texture");
+		
 	}
+	
+	/* Methodes */
+	//public final void dessiner()
+	protected void dessiner()
+	{
+		System.out.println("Dessin d'une figure quadrilatere");
+	}
+
+
+	
 
 
 }
--- Java/Hierarchie/src/geometrie/Rectangle.java
+++ Java/Hierarchie/src/geometrie/Rectangle.java
@@ -2,17 +2,47 @@ package geometrie;
 
 public class Rectangle extends Quadrilatere 
 {
+   int code;
+   
+   /* Getters */
+   public double getPerimetre()
+   {
+	   System.out.println("Calcul du périmètre d'un rectangle");
+	   double perimetre = 0.0;
+	   Segment[] segments = this.getLesSegments();
+	   perimetre = segments[0].longueur() * 2 + segments[1].longueur() * 2;
+	   return perimetre;
+   }
+   
+   /* Constructeurs */
    public Rectangle()
    {
 	   // Constructeur par defaut
+	   this.code = 2;
 	   System.out.println("\nConstructeur de Rectangle");
    }
    
-   public Rectangle(Point p1, Point p2)
+   public Rectangle(Point hg, Point bd)
    {
-	   Point p3 = new Point(p1.getX(), p2.getY());
-	   Point p4 = new Point(p2.getX(), p1.getY());
-	   
-	   // Quadrilatere(p1, p2, p3, p4);
+	   super(hg, new Point(hg.getX(), bd.getY()), bd, new Point(bd.getX(), hg.getY()));
+	   this.code = 2;
+   }
+   
+   public Rectangle(Point hg, Point bd, String couleur, String texture)
+   {
+	   this(hg, bd);
+	   this.setCouleur(couleur);
+	   this.setTexture(texture);
+   }
+   
+   /* Methodes */
+   public void dessiner()
+   {
+	   System.out.println("Dessin d'une figure rectangulaire");
+   }
+   
+   public void dessiner(Point p)
+   {
+	   System.out.println("Dessin d'un rectangle");
    }
 }
