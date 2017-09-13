0d69899f0e9141834a4cd4385f77ed0864d97bb6
Luu Truong Thanh <tluu@SA203-07A.ensi-bourges.fr>
2017-09-13 17:58:36 +0200

    Cesar minuscule

--- STI4A/cryptographie/TD01/cesar_crypt.c
+++ STI4A/cryptographie/TD01/cesar_crypt.c
@@ -11,8 +11,25 @@ int main(int argc, char const *argv[])
 	FILE *fi, *fo;
 	char *cp;
 	int c;
+	int cle;
 
-	cp = "test pmol";
-	printf("%s\n", cp[7]);
+	cle = 27;
+	cle %= 26;
+
+	/* a = 97, z = 122 */
+	/* A = 65, Z = 90 */
+	/* Ca fonctionne pour les minuscules */
+	printf("%c\n", (('a'+cle)%97)+'a');
+
+	/* 
+	 *  Il faut maintenant bouclier pour encoder chaque caractere
+	 *  + verifier que le caractere est bien une lettre codable
+	 *  + verifier si c'est une minuscule ou une majuscule
+	 */
+
+	printf("%d\n", 'A');
+	printf("%d\n", 'Z');
+	printf("%d\n", 'z');
+
 	return 1;
 }
