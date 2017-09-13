a16c009ff030bbdb94fd907c6794e1d3c3bddf7a
Luu Truong Thanh <tluu@SA203-07A.ensi-bourges.fr>
2017-09-13 17:38:05 +0200

    Cesar

---
+++ STI4A/cryptographie/TD01/cesar_crypt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *
+ *  Usage xor_crypt key input_file output_file
+ *
+ */
+
+int main(int argc, char const *argv[])
+{
+	FILE *fi, *fo;
+	char *cp;
+	int c;
+
+	cp = "test pmol";
+	printf("%s\n", cp[7]);
+	return 1;
+}
