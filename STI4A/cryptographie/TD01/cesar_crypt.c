6c208fe6acd773797fa7461a9534539309f2c0b3
Thanh <Ikau@github.com>
2017-09-14 15:44:31 +0200

    Version finale cesar

--- STI4A/cryptographie/TD01/cesar_crypt.c
+++ STI4A/cryptographie/TD01/cesar_crypt.c
@@ -3,7 +3,7 @@
 
 /**
  *
- *  Usage cesar_crypt key input_file output_file
+ *  Usage cesar_crypt key input_file output_file [decrypt]
  *
  */
 
@@ -16,10 +16,11 @@ int main(int argc, char const *argv[])
 	/* A = 65, Z = 90 */
 
 	/* Verification du nombre d'arguments */
-	if(argc != 4)
+	if(argc != 4 && argc != 5)
 	{
-		printf("Erreur de syntaxe\nUsage : cesar_crypt key input_file output_file\n");
-		printf("key         : int\ninput_file  : filename\noutput_file : filename\n");
+		printf("Erreur de syntaxe\nUsage : cesar_crypt key input_file output_file [decrypt]\n");
+		printf("key         : integer\ninput_file  : filename\noutput_file : filename\n");
+		printf("decrypt     : str [optional]\n");
 		return 1;
 	}
 
@@ -29,29 +30,43 @@ int main(int argc, char const *argv[])
 		{
 			if((fo = fopen(argv[3], "wb")) != NULL)
 			{
-				/* Meme si l'on rentre un integer en console, on recoit un char* */
-				/* Il faut donc utiliser atoi() pour avoir la valeur numerique */
-				/* On modifie la cle pour qu'elle ne soit pas superieure a 25 */
-				cle = atoi(argv[1])%26;
+				/* 
+				 * Meme si l'on rentre un integer en console, on recoit un char*
+				 * Il faut donc utiliser atoi() pour avoir la valeur numerique
+				 * On modifie la cle pour qu'elle ne soit pas superieure a 25 
+				 */
+
+				if(argc == 4)
+				{
+					cle = atoi(argv[1])%26;
+					printf("Encryptage...\n");
+				}
+				else
+				{
+					cle = -(atoi(argv[1])%26);
+					printf("Decryptage...\n");
+				}
 
 				while((c=getc(fi)) != EOF)
 				{
-					/* Si minuscules */
 					if('a' <= c && c <= 'z')
 					{
 						/* Explications
-						 * c+cle est le chiffrage attendu
+						 * c+cle est le chiffrage/dechiffrage attendu
 						 * %97 donne la position par rapport a 'a'
 						 * %26 exprime une circulaire dans l'alphabet
 						 */
 						c = (((c+cle)%97)%26)+'a';
 					}
-					/* Sinon si majuscule */
 					else if('A' <= c && c <= 'Z')
 					{
 						c = (((c+cle)%65)%26)+'A';
 					}
-					/* Sinon rien : on reecrit directement */
+					else if(c != ' ') /* Trop facile casser si on code les espaces */
+					{
+						/* Sinon caractere speciale : on encrypte naivement */
+						c += cle;
+					}
 					putc(c, fo);
 				}
 				fclose(fo);
@@ -72,5 +87,6 @@ int main(int argc, char const *argv[])
 		printf("Erreur de lecture de la clé\n");
 	}
 
+	printf("Fin du programme\n");
 	return 1;
 }
