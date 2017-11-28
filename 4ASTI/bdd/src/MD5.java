aec29a5fd495b447777909fd8dbf5a16670e07ca
Thanh <tlutru@gmail.com>
2017-11-28 18:00:38 +0100

    BDD TD2 fin

---
+++ 4ASTI/bdd/src/MD5.java
@@ -0,0 +1,35 @@
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5 {
+	
+	private String element;
+	private MessageDigest md;
+	private byte[] digest;
+	
+	final int MINIMUM_BYTE = 128;
+	
+	
+	public MD5() throws NoSuchAlgorithmException
+	{
+		this.md = MessageDigest.getInstance("MD5");
+	}
+	
+	public void update(String element)
+	{
+		this.md.update(element.getBytes());
+	}
+	
+	public void digest()
+	{
+		this.md.update(this.element.getBytes());
+		this.digest = this.md.digest();
+	}
+	
+	public int hacheK(int k)
+	{
+		return this.digest[k] + this.MINIMUM_BYTE;
+	}
+
+}
