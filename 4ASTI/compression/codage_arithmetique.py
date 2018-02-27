8d2dc49b3d2fd3b558168b9518f3be3893999c2d
Thanh <tlutru@gmail.com>
2018-02-27 14:14:24 +0100

    Compression: add coder class; rename tmp.py into test.py

--- 4ASTI/compression/codage_arithmetique.py
+++ 4ASTI/compression/codage_arithmetique.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3.5
 
 # ========================
-#           TODO
+#          TODO
 # ========================
 # Faire une classe
 #
 
-"""
-class CodeurArithmetique(object):
-	Implementation du codage arithmetique. Permet de decoder et de decoder
 
-	def __init__(self, tab_prob, interv_lettres):
-
-		# Initialisation des probabilites
-
-		# Init des intervalles
-"""
+# ========================
+#         GLOBALS
+# ========================
 
 # e, s, i, p
-interv = [0.0, 0.4, 0.6, 0.8, 1.0]
-probs  = [0.4, 0.2, 0.2, 0.2]
-encodage = "esip"
+INTERV   = [0.0, 0.4, 0.6, 0.8, 1.0]
+PROBS    = [0.4, 0.2, 0.2, 0.2]
+ENCODAGE = "esip"
 
 # e, n, t, v, .
 #interv = [0.0, 0.3, 0.6, 0.8, 0.9, 1.0]
@@ -40,57 +34,135 @@ dic = {
 	'.':(0.9, 1.0, 4)
 }
 
-def code(inf, sup):
-	out  = [inf]
-	last = inf
-	for p in probs:
-		borne = (sup-inf)*p + last
-		last  = borne
-		out.append(borne)
-	out.append(sup)
-	return out
-#END_DEF
 
-def decode(coded, taille):
-	out    = ""
-	nombre = coded
+# ========================
+#          CLASS
+# ========================
 
-	for _ in range(taille):
-		for i in range(len(interv)-1):
-			if( nombre >= interv[i] and nombre < interv[i+1] ):
-				out += encodage[i]
-				nombre = (nombre - interv[i]) / probs[i]
-				break
+class CodeurArithmetique(object):
+	"""Implementation du codage arithmetique. Permet de decoder et de decoder"""
+
+	def __init__(self, probs=[], intervals=[], chars=""):
+		self.probs     = probs
+		self.intervals = intervals
+		self.encodage  = chars
+		self.dict      = self.init_dict(chars)
+	#END_DEF
+
+	########################
+	##### DICTIONNAIRES ####
+	########################
+
+	def init_dict(self, chars):
+		dic = {}
+		for i in range(len(chars)):
+			dic[chars[i]] = (self.intervals[i], self.intervals[i+1], i)
 		#END_FOR
-	#END_FOR
 
-	return out
-#END_DEF
+		return dic
+	#END_DEF
 
+	def create_dict(self, text):
+	#END_DEF
+
+	########################
+	####### ENCODING #######
+	########################
+
+	def encode_intervals(self, inf, sup):
+		coded = [inf]
+
+		last  = inf
+		for p in self.probs:
+			bound = (sup-inf)*p + last
+			coded.append(bound)
+			last  = bound
+		#END_FOR
+		coded.append(sup)
+
+		return coded
+	#END_DEF
+
+	def decode(self, nombre, size):
+		decoded = ""
+		coded   = nombre
+
+		for _ in range(size):
+			for i in range(len(self.intervals)):
+				if( coded >= self.intervals[i] and coded < self.intervals[i+1] ):
+					decoded += self.encodage[i]
+					coded   = (coded - self.intervals[i]) / self.probs[i]
+					break
+			#END_FOR
+		#END_FOR
+
+		return decoded
+	#END_DEF
+
+	def encode(self, word):
+		coded = self.intervals
+
+		for i in range(len(word)):
+			character = word[i]
+			index_inf = self.dict[character][2]
+			index_sup = self.dict[character][2] + 1
+
+			infimum   = coded[index_inf]
+			supremum  = coded[index_sup]
+			coded     = self.encode_intervals(infimum, supremum)
+		#END_FOR
+
+		return (coded[0], coded[-1], len(word))
+	#END_DEF
+
+
+	########################
+	####### Fichiers #######
+	########################
+
+	def encode_file(self, src, dst):
+		with open(src, 'r') as fSrc:
+			# Parcours du fichier, creation du dictionnaire
+			self.dict = self.create_dict(fSrc.read())
+
+			# Compression du fichier
+			fSrc.seek(0)
+			with open(dst, 'w+b') as fDst:
+				for mot in fSrc.read().split(' '):
+					coded = self.encode(mot)
+
+					mean   = (coded[0] + coded[1]) / 2
+					taille = coded[2]
+
+					# write encoded to file
+					#fDst.write()
+				#END_FOR
+			#END_WITH
+		#END_WITH
+	#END_DEF
+
+	def decode_file(self, src, dst):
+		with open(src, 'r') as f:
+			pass
+		#END_WITH
+	#END_DEF
+#END_CLASS
 
 # ====================
 #        TEST
 # ====================
-#mot = "vent."
-mot = "esipe"
+def main():
+	#mot = "vent."
+	mot = "esipe"
 
+	c1 = CodeurArithmetique()
 
-out = []
-out.append(interv)
-for i in range(len(mot)):
-	character = mot[i]
-	index_inf = dic[character][2]
-	index_sup = dic[character][2]+1
+	codeur      = CodeurArithmetique(PROBS, INTERV, ENCODAGE)
+	test_encode = codeur.encode(mot)
+	test_decode = codeur.decode(0.221, 5)
+	print("Le codage arithmetique de `%s` est [%f, %f]" % (mot, test_encode[0], test_encode[1]))
+	print("Le decodage de `0.221` est `%s`" % (test_decode))
+#END_MAIN
 
-	borne_inf = out[i][index_inf]
-	borne_sup = out[i][index_sup]
-	out.append(code(borne_inf, borne_sup))
-
-for intevalles in out:
-	print(intevalles)
-
-print("")
-test_decode = decode(0.221, 5)
-print("Le codage arithmetique de `%s` est [%f, %f]" % (mot, out[-1][0], out[-1][-1]))
-print("Le decodage de 0.221 est `%s`" % (test_decode))
-
+if __name__ == '__main__':
+	main()
--- memo/tmp.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-D = {u'Label0':26, u'Label1': 17, u'Label2':30}
-
-plt.bar(range(len(D)), D.values(), align='center')
-plt.xticks(range(len(D)), D.keys())
-plt.show()
