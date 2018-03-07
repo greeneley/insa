26b616bd0aea3de070a0fd38cf4d9d5cff232663
Thanh <tlutru@gmail.com>
2018-03-07 01:21:54 +0100

    Add plan(rayon, draw, shine), test ombre, add commentaires

--- 4ASTI/synthese/LanceRayons/shader.fs
+++ 4ASTI/synthese/LanceRayons/shader.fs
@@ -4,7 +4,16 @@ precision mediump float;
 
 varying vec3 pixCenter;
 
-// ============= STRUCT =============
+
+/* 
+	#################################
+	#################################
+				STRUCTURES
+	#################################
+	#################################
+*/
+
+// ==================================
 struct ray
 {
 	vec3 O; // origine du rayon
@@ -12,11 +21,15 @@ struct ray
 	float t;
 };
 
+// ==================================
 struct plan
 {
-	vec3 O; // quelconque appartenant au plan
+	vec3 S; // quelconque appartenant au plan
 	vec3 n; // vecteur normal au plan
+	vec3 color;
 };
+
+// ==================================
 struct sphere
 {
 	vec3  C;     // Centre du cercle
@@ -24,6 +37,7 @@ struct sphere
 	float r;     // Rayon
 };
 
+// ==================================
 struct source
 {
 	vec3 pos; // Position de la source
@@ -31,43 +45,100 @@ struct source
 
 };
 
-// ============= GLOBAL =============
-int NB_SOURCES = 2;
 
 /* 
-	##################################
-	##################################
-				  PLAN
-	##################################
-	################################## 
+	#################################
+	#################################
+				GLOBAL
+	#################################
+	################################# 
+*/
+int   NB_SOURCES = 0;
+float PI         = 3.14;
+
+
+/* 
+	#################################
+	#################################
+			FONCTIONS PLAN
+	#################################
+	################################# 
 */
 
 // ==================================
 float rayPlan(ray r, plan p)
 {
-	//TODO
-	return -1.0;
-}
-
-
-// ==================================
-float draw_plan(ray r, plan p)
-{
-	//TODO
+	vec3 OS     = p.S - r.O; // Vecteur OS
+	float denom = dot(p.n, r.V);
+	if(denom != 0.0)
+	{
+		float t = (dot(OS, p.n) / denom);
+		return t;
+	}
 	return -1.0;
 }
 
 // ==================================
-float shine_plan(ray r, plan p)
+int draw_plan(ray r, plan p)
 {
-	//TODO
-	return -1.0;
+	r.t = rayPlan(r, p);
+	if(r.t >= 0.0)
+	{
+		gl_FragColor = vec4(p.color, 1.0);
+		return 1;
+	}
+	return -1;
 }
+
+// ==================================
+void shine_plan(ray r, plan p, source t_src[10])
+{
+	r.t = rayPlan(r, p);
+	if(r.t >= 0.0)
+	{
+		// Init de la luminance resultante
+		vec3 L0 = vec3(0.0, 0.0, 0.0);
+
+		// Calcul de la luminance resultatne
+		for(int i=0; i<1000; i++)
+		{
+			// Pour eviter a devoir changer la taille de la loop
+			if(i>=NB_SOURCES)
+			{
+				break;
+			}
+
+			// Eclairage
+			vec3   I        = r.O + r.t*r.V;
+			vec3   N        = normalize(I - p.S);
+			vec3   V        = normalize(t_src[i].pos - I);
+			float cosThetaI = dot(N,V);
+			
+			// Brillance
+			vec3 V0         = normalize(-r.V);
+			vec3 H          = normalize(V0+V);
+			float cosAlphaI = dot(H,N);
+
+			// Calcul de la reflectance
+			vec3 ks = vec3(0.1, 0.1, 0.1);
+			vec3 kd = p.color;
+			float n = 5.0;
+			vec3 reflectance = (kd/PI) + ((n+2.0)/(2.0*PI))*ks*pow(cosAlphaI, n);
+
+			// Addition d'une source de lumiere
+			L0 = L0 + t_src[i].pow * reflectance * cosThetaI; 
+		}
+		
+		// L0 est au final la somme de toutes les luminances	
+		gl_FragColor = vec4(L0, 1.0);
+	}
+}
+
 
 /* 
 	##################################
 	##################################
-				 SPHERES
+			FONCTIONS SPHERE
 	##################################
 	################################## 
 */
@@ -121,20 +192,19 @@ void draw_sphere(ray r, sphere s)
 void shine_sphere(ray r, sphere s, source t_src[10])
 {
 	r.t = raySphere(r, s);
-	if(r.t >= 0.0) // ne touche pas
+	if(r.t >= 0.0)
 	{
 		// Init de la luminance resultante
 		vec3 L0 = vec3(0.0, 0.0, 0.0);
 
 		// Calcul de la luminance resultatne
-		for(int i=0; i<1000; i++)
+		for(int i=0; i<10; i++)
 		{
+			// Pour eviter a devoir changer la taille de la loop
 			if(i>=NB_SOURCES)
 			{
 				break;
 			}
-
-			float PI = 3.14;
 
 			// Eclairage
 			vec3   I        = r.O + r.t*r.V;
@@ -157,9 +227,33 @@ void shine_sphere(ray r, sphere s, source t_src[10])
 			L0 = L0 + t_src[i].pow * reflectance * cosThetaI; 
 		}
 		
+		// L0 est au final la somme de toutes les luminances	
 		gl_FragColor = vec4(L0, 1.0);
 	}
 }
+
+// ==================================
+void ombre_sphere(source t_src[10], sphere s, plan p)
+{
+	for(int i=0; i<10; i++)
+	{
+		if(i >= NB_SOURCES)
+		{
+			break;
+		}
+
+		ray r = ray(t_src[i].pos, pixCenter, -1.0);
+		r.t   = rayPlan(r, p);
+		if(r.t >= 0.0)
+		{
+			// Si le rayon passe par le cercle
+			//gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+		}
+	}
+	
+}
+
+
 
 // ==================================
 void main(void) 
@@ -167,21 +261,37 @@ void main(void)
 	// Par defaut la scene est blanche
 	gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0) ;
 
-	// Initialisation des structs
+	// Init des structs
 	ray    rayon  = ray(vec3(0.0, 0.0, 0.0), pixCenter, -1.0);
-	sphere sph1   = sphere(vec3(-50.0, 200.0, 0.0), vec3(0.8, 0.1, 0.1), 25.0);
-	sphere sph2   = sphere(vec3(20.0, 150.0, -20.0), vec3(0.1, 0.1, 0.8), 10.0);
 
-	// Sources de lumiere
+	sphere sph1   = sphere(vec3(-10.0, 40.0, 0.0), vec3(0.8, 0.1, 0.1), 5.0);
+	sphere sph2   = sphere(vec3(10.0, 40.0, 0.0), vec3(0.1, 0.1, 0.8), 5.0);
+
+	plan   plan1  = plan(vec3(0.0, 0.0, -1.0), vec3(0.0, 0.0, 1.0), vec3(0.6, 0.3, 0.2));
+	//plan   plan2  = plan(vec3(0.0, 0.0, 40.0), vec3(0.0, -1.0, 0.0), vec3(0.1, 0.1, 0.9));
+
+	// Init des sources de lumiere
 	source t_src[10];
-	t_src[0] = source(vec3(0.0, 130.0, 150.0), vec3(10.0, 10.0, 10.0));
-	t_src[1] = source(vec3(30.0, 130.0, -150.0), vec3(10.0, 10.0, 10.0));
+	t_src[0] = source(vec3(-20.0, 35.0, 20.0), vec3(7.0, 7.0, 7.0));
+	t_src[1] = source(vec3(20.0, 35.0, 20.0), vec3(7.0, 7.0, 7.0));
+	NB_SOURCES = 2;
 
 	// Dessin
+	draw_plan(rayon, plan1);
+	//draw_plan(rayon, plan2);
+
 	draw_sphere(rayon, sph1);
 	draw_sphere(rayon, sph2);
 
 	// Eclairage
+	//shine_plan(rayon, plan2, t_src);
+	shine_plan(rayon, plan1, t_src);
+
 	shine_sphere(rayon, sph1, t_src);
 	shine_sphere(rayon, sph2, t_src);
+
+	// Ombre
+	ombre_sphere(t_src, sph1, plan1);
+	ombre_sphere(t_src, sph2, plan1);
+
 }
