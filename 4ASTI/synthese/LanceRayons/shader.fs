4f6cf165cfe9098589ff9f3cba5f7f09d4aa1fc1
Thanh <tlutru@gmail.com>
2018-03-08 03:32:16 +0100

    Fix ombres, restructuration du code, nettoyage

--- 4ASTI/synthese/LanceRayons/shader.fs
+++ 4ASTI/synthese/LanceRayons/shader.fs
@@ -104,50 +104,62 @@ float raySphere(ray r, sphere s)
 		float t1 = (-B - sqrt(delta)) / (2.0 * A);
 		float t2 = (-B + sqrt(delta)) / (2.0 * A);
 
-		if(t1 < 0.0 && t2 < 0.0)
-		{
-			return -1.0;
-		}
-		else if(t1 < 0.0)
-		{
-			return t2;
-		}
-		else if(t2 < 0.0)
-		{
-			return t1;
-		}
-		else
-		{
-			return min(t1,t2);
-		}
+		if(t1 < 0.0 && t2 < 0.0) return -1.0;
+		else if(t1 < 0.0)        return t2;
+		else if(t2 < 0.0)        return t1;
+		else                     return min(t1,t2);
 	}
 	return -1.0;
 }
 
 // ==================================
-vec3 eclaire_ombre_point(ray r_src, vec3 pt_repere, source src_lum, vec3 kd)
+void eclaire_ombre_point(ray r_src, vec3 pt_repere, vec3 kd)
 {
-	vec3  Li     = vec3(0.0, 0.0, 0.0);
-	vec3  I      = r_src.O + r_src.t*r_src.V;
-	ray   rayon  = ray(I, src_lum.pos, -1.0);
-	bool  hidden = false;
-	float t;
+	vec3 LO       = vec3(0.0, 0.0, 0.0);
+	vec3 I        = r_src.O + r_src.t*r_src.V;
+	bool shadowed = false;
 
-	for(int i=0; i<NB_SPHERES; i++)
+	// Pour chaque rayon de source de lumiere...
+	for(int i=0; i<1000; i++)
 	{
-		t = raySphere(rayon, T_SPH[i]);
-		if( (t >= 0.0) && (pt_repere != T_SPH[i].C))
+		if(i>=NB_SOURCES) break;
+
+		ray r_lum = ray(I, T_SRC[i].pos, -1.0);
+		shadowed = false;
+
+		// ... on verifie si une sphere le bloque ...
+		for(int j=0; j<1000; j++)
 		{
-			hidden = true;
-			break;
-		}
-	}
+			if(j>=NB_SPHERES) break;
 
-	if(!hidden)
-	{
+			r_lum.t  = raySphere(r_lum, T_SPH[j]);
+			if( (r_lum.t >= 0.0) && (pt_repere != T_SPH[j].C) )
+			{
+				shadowed = true;
+				break;
+			}
+		}
+
+		if(shadowed) break;
+		// ... ou si un plan le bloque ...
+		for(int j=0; j<1000; j++)
+		{
+			if(j>=NB_PLANS) break;
+
+			r_lum.t = rayPlan(r_lum, T_PLANS[j]);
+			if( (r_lum.t >= 0.0) && (pt_repere != T_PLANS[j].S) )
+			{
+				shadowed = true;
+				break;
+			}
+		}
+
+		if(shadowed) break;
+		// .. sinon on calcule la luminance resultante
+
 		// Eclairage
 		vec3   N        = normalize(I - pt_repere);
-		vec3   V        = normalize(src_lum.pos - I);
+		vec3   V        = normalize(T_SRC[i].pos - I);
 		float cosThetaI = dot(N,V);
 		
 		// Brillance
@@ -155,206 +167,179 @@ vec3 eclaire_ombre_point(ray r_src, vec3 pt_repere, source src_lum, vec3 kd)
 		vec3 H          = normalize(V0+V);
 		float cosAlphaI = dot(H,N);
 
-		// Reflectance
+		// Calcul de la reflectance
 		vec3 ks = vec3(0.1, 0.1, 0.1);
 		float n = 5.0;
 		vec3 reflectance = (kd/PI) + ((n+2.0)/(2.0*PI))*ks*pow(cosAlphaI, n);
 
-		// Luminance issue de la source de lumiere
-		Li = src_lum.pow * reflectance * cosThetaI; 
+		// Addition d'une source de lumiere
+		LO += T_SRC[i].pow * reflectance * cosThetaI; 
 	}
-
-	return Li;
-}
-
-/* 
-	#################################
-	#################################
-			FONCTIONS PLAN
-	#################################
-	################################# 
-*/
-// ==================================
-int draw_plan(ray r, plan p)
-{
-	r.t = rayPlan(r, p);
-	if(r.t >= 0.0)
-	{
-		gl_FragColor = vec4(p.color, 1.0);
-		return 1;
-	}
-	return -1;
+	
+	// L0 est au final la somme de toutes les luminances recue
+	gl_FragColor = vec4(LO, 1.0);
 }
 
 // ==================================
-void shine_plan(ray r, plan p)
+void draw_raw_all(ray rayon)
 {
-	r.t = rayPlan(r, p);
-	if(r.t >= 0.0)
-	{
-		// Init de la luminance resultante
-		vec3 LO = vec3(0.0, 0.0, 0.0);
-
-		// Calcul de la luminance resultante
-		for(int i=0; i<NB_SOURCES; i++)
-		{
-
-			// Test d'ombre avec tous les objets
-			LO += eclaire_ombre_point(r, p.S, T_SRC[i], p.color);
-
-			/*/ Pour eviter a devoir changer la taille de la loop
-			if(i>=NB_SOURCES)
-			{
-				break;
-			}
-
-			// Eclairage
-			vec3 I = r.O + r.t*r.V;
-
-			// On verifie si il y a une ombre ou non
-			for(int j=0; j<100; j++)
-			{
-				if(j >= NB_SPHERES)
-				{
-					break;
-				}
-
-				if(s.C != T_SPH[j].C)
-				{
-					ray   r_sph = ray(I, T_SRC[i].pos, -1.0);
-					float T_SPH = raySphere(r_sph, T_SPH[j]);
-					if(T_SPH >= 0.0)
-					{
-						LO = vec3(0.0, 0.0, 0.0);
-					}
-				}
-				else
-				{
-					vec3   N        = normalize(I - s.C);
-					vec3   V        = normalize(T_SRC[i].pos - I);
-					float cosThetaI = dot(N,V);
-
-					// Vecteurs brillance
-					vec3 V0         = normalize(-r.V);
-					vec3 H          = normalize(V0+V);
-					float cosAlphaI = dot(H,N);
-
-					// Calcul de la reflectance
-					vec3 ks = vec3(0.1, 0.1, 0.1);
-					vec3 kd = s.color;
-					float n = 5.0;
-					vec3 reflectance = (kd/PI) + ((n+2.0)/(2.0*PI))*ks*pow(cosAlphaI, n);
-
-					// Addition d'une source de lumiere
-					LO = LO + T_SRC[i].pow * reflectance * cosThetaI;
-				}
-
-			}
-			*/
-
-		}
-
-		// LO est au final la somme de toutes les luminances
-		gl_FragColor = vec4(LO, 1.0);
-	}
-}
-
-
-/* 
-	##################################
-	##################################
-			FONCTIONS SPHERE
-	##################################
-	################################## 
-*/
-
-// ==================================
-void draw_sphere(ray r, sphere s)
-{
-	r.t = raySphere(r, s);
-	if(r.t >= 0.0)
-	{
-		gl_FragColor = vec4(s.color, 1.0);
-	}
-}
-
-// ==================================
-void shine_sphere(ray r, sphere s)
-{
-	r.t = raySphere(r, s);
-	if(r.t >= 0.0)
-	{
-		// Init de la luminance resultante
-		vec3 LO = vec3(0.0, 0.0, 0.0);
-
-		// Calcul de la luminance resultante
-		for(int i=0; i<NB_SOURCES; i++)
-		{
-			// Test d'ombre avec tous les objets
-			LO += eclaire_ombre_point(r, s.C, T_SRC[i], s.color);
-		}
-		
-		// LO est au final la somme de toutes les luminances	
-		gl_FragColor = vec4(LO, 1.0);
-	}
-}
-
-// ==================================
-void draw_raw_spheres(ray rayon)
-{
-	bool   min_init = false;
-	float  min_t    = 0.0;
-	float  tmp_t    = 0.0;
+	bool   min_init_s = false;
+	bool   min_init_p = false;
+	float  min_t_s    = 0.0;
+	float  min_t_p    = 0.0;
+	float  tmp_t      = 0.0;
 	sphere min_sph;
+	plan   min_plan;
+
 
 	// Recherche du t min et de la sphere s associee
  	for(int i=0; i<100; i++)
 	{
 		if(i>=NB_SPHERES) break;
 
-		if(!min_init) // Recherche premiere valeur positive
+		if(!min_init_s) // Recherche premiere valeur positive
 		{
-			min_t = raySphere(rayon, T_SPH[i]);
-			if(min_t >= 0.0)
+			min_t_s = raySphere(rayon, T_SPH[i]);
+			if(min_t_s >= 0.0)
 			{
 				min_sph  = T_SPH[i];
-				min_init = true;
+				min_init_s = true;
 			}
 		}
 		else // Recherche du min definitif
 		{
 			tmp_t = raySphere(rayon, T_SPH[i]);
-			if((tmp_t >= 0.0) && (tmp_t < min_t))
+			if((tmp_t >= 0.0) && (tmp_t < min_t_s))
 			{
-				min_t   = tmp_t;
+				min_t_s   = tmp_t;
 				min_sph = T_SPH[i];
 			}
 		}		
 	}
 
-	if(min_init)
+	// Recherche du t min et du plan associe
+ 	for(int i=0; i<100; i++)
 	{
-		gl_FragColor = vec4(min_sph.color, 1.0);
-	}
-	else
-	{
-		gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+		if(i>=NB_PLANS) break;
+
+		if(!min_init_p) // Recherche premiere valeur positive
+		{
+			min_t_p = rayPlan(rayon, T_PLANS[i]);
+			if(min_t_p >= 0.0)
+			{
+				min_plan   = T_PLANS[i];
+				min_init_p = true;
+			}
+		}
+		else // Recherche du min definitif
+		{
+			tmp_t = rayPlan(rayon, T_PLANS[i]);
+			if((tmp_t >= 0.0) && (tmp_t < min_t_p))
+			{
+				min_t_p  = tmp_t;
+				min_plan = T_PLANS[i];
+			}
+		}		
 	}
 
+	if(min_init_p && min_init_s) // min trouve chez les deux types
+	{
+		if(min_t_s < min_t_p) gl_FragColor = vec4(min_sph.color,  1.0);
+		else                  gl_FragColor = vec4(min_plan.color, 1.0);
+	}
+	else if(min_init_s)       gl_FragColor = vec4(min_sph.color,  1.0);
+	else if(min_init_p)       gl_FragColor = vec4(min_plan.color, 1.0);
+	else                      gl_FragColor = vec4(0.0, 0.0, 0.0,  1.0);
 }
 
 // ==================================
-void draw_shined_spheres(ray rayon)
+void draw_shined_all(ray rayon)
 {
-	for(int i=0; i<100; i++)
+	bool   min_init_s = false;
+	bool   min_init_p = false;
+	float  min_t_s    = 0.0;
+	float  min_t_p    = 0.0;
+	float  tmp_t      = 0.0;
+	sphere min_sph;
+	plan   min_plan;
+
+
+	// Recherche du t min et de la sphere s associee
+ 	for(int i=0; i<100; i++)
 	{
-		if(i>=NB_SPHERES)
+		if(i>=NB_SPHERES) break;
+
+		if(!min_init_s) // Recherche premiere valeur positive
 		{
-			break;
+			min_t_s = raySphere(rayon, T_SPH[i]);
+			if(min_t_s >= 0.0)
+			{
+				min_sph  = T_SPH[i];
+				min_init_s = true;
+			}
 		}
-		shine_sphere(rayon, T_SPH[i]);
+		else // Recherche du min definitif
+		{
+			tmp_t = raySphere(rayon, T_SPH[i]);
+			if((tmp_t >= 0.0) && (tmp_t < min_t_s))
+			{
+				min_t_s   = tmp_t;
+				min_sph = T_SPH[i];
+			}
+		}		
 	}
+
+	// Recherche du t min et du plan associe
+ 	for(int i=0; i<100; i++)
+	{
+		if(i>=NB_PLANS) break;
+
+		if(!min_init_p) // Recherche premiere valeur positive
+		{
+			min_t_p = rayPlan(rayon, T_PLANS[i]);
+			if(min_t_p >= 0.0)
+			{
+				min_plan   = T_PLANS[i];
+				min_init_p = true;
+			}
+		}
+		else // Recherche du min definitif
+		{
+			tmp_t = rayPlan(rayon, T_PLANS[i]);
+			if((tmp_t >= 0.0) && (tmp_t < min_t_p))
+			{
+				min_t_p  = tmp_t;
+				min_plan = T_PLANS[i];
+			}
+		}		
+	}
+
+	if(min_init_p && min_init_s) // min trouve chez les deux types
+	{
+		if(min_t_s < min_t_p) 
+		{
+			rayon.t = min_t_s;
+			eclaire_ombre_point(rayon, min_sph.C,  min_sph.color);
+		}
+		else
+		{
+			rayon.t = min_t_p;
+			eclaire_ombre_point(rayon, min_plan.S, min_plan.color);
+		}
+	}
+	else if(min_init_s)
+	{
+		rayon.t = min_t_s;
+		eclaire_ombre_point(rayon, min_sph.C,  min_sph.color);
+	}
+	else if(min_init_p)
+	{
+		rayon.t = min_t_p;
+		eclaire_ombre_point(rayon, min_plan.S, min_plan.color);
+	}       
+	else gl_FragColor = vec4(0.0, 0.0, 0.0,  1.0);
 }
+
 /*
 	##################################
 	##################################
@@ -391,86 +376,26 @@ void scene_Test(void)
 	//T_SPH[2]   = sphere(lumiere, vec3(0.1, 0.1, 0.8), 1.0);
 
 	// ===== Dessin
-	for(int i=0; i<NB_PLANS; i++)
-	{
-		draw_plan(rayon, T_PLANS[i]);
-	}
-
-	for(int i=0; i<NB_SPHERES; i++)
-	{
-		draw_sphere(rayon, T_SPH[i]);
-	}
-
-	// ===== Eclairage
-	for(int i=0; i<NB_PLANS; i++)
-	{
-		//shine_plan(rayon, T_PLANS[i]);
-	}
-
-	for(int i=0; i<NB_SPHERES; i++)
-	{
-		shine_sphere(rayon, T_SPH[i]);
-	}
+	draw_raw_all(rayon);
+	//draw_shined_all(rayon);
 }
 
-// ===================================
-/*/// La scene represente une salle ou se situe une boule
-void scene_2(void) 
-{
-	// ===== Par defaut la scene est blanche
-	gl_FragColor  = vec4(1.0, 1.0, 1.0, 1.0) ;
-
-	// ===== Init des structs
-	ray    rayon  = ray(vec3(0.0, 0.0, 0.0), pixCenter, -1.0);
-
-	// Plans
-	T_PLANS[0] = plan(vec3(  0.0, 20.0,  0.0), vec3( 0.0, -1.0, 0.0), vec3(0.5, 0.5, 0.5));
-	T_PLANS[1] = plan(vec3(  0.0, 20.0, -100.0), vec3( 0.0,  0.0, 1.0), vec3(0.8, 0.2, 0.2));
-	T_PLANS[2] = plan(vec3( 50.0, 20.0,   0.0), vec3(-1.0,  0.25, 0.0), vec3(0.2, 0.8, 0.2));
-	T_PLANS[3] = plan(vec3(-50.0, 20.0,   0.0), vec3( 1.0,  0.25, 0.0), vec3(0.2, 0.2, 0.8));
-
-	// Sources de lumieres
-	T_SRC[0] = source(vec3(10.0, 15.0, 15.0), vec3(10.0, 10.0, 10.0));
-
-	// Spheres
-	T_SPH[0] = sphere(vec3(0.0, 10.0, 0.0), vec3(0.8, 0.1, 0.1), 1.0);
-
-	// ===== Dessin
-	for(int i=0; i<NB_PLANS; i++)
-	{
-		//draw_plan(rayon, T_PLANS[i]);
-	}
-
-	for(int i=0; i<NB_SPHERES; i++)
-	{
-		//draw_sphere(rayon, T_SPH[i]);
-	}
-
-	// ===== Eclairage
-	for(int i=0; i<NB_PLANS; i++)
-	{
-		shine_plan(rayon, T_PLANS[i]);
-	}
-
-	for(int i=0; i<NB_SPHERES; i++)
-	{
-		shine_sphere(rayon, T_SPH[i]);
-	}
-}
-
-
-/*/// ==================================
+// ==================================
 
 void scene_SateliteOrbital(void) // Manque rotation orbitale  float(uTime) modulo
 {
 	// ===== Par defaut la scene est noire
 	gl_FragColor  = vec4(0.1, 0.1, 0.1, 1.0) ;
 
-	// Init des structs
+	// ===== Init des structs
 	ray    rayon  = ray(uOriVector, pixCenter, -1.0);
 
+	// Plans
+	NB_PLANS      = 0;
+
+	// Spheres
 	//Planete Bleue
-	sphere sph0   = sphere(vec3(-10.0, 1000.0, 0.0), vec3(0.0, 0.0, 0.8), 50.0);
+	sphere sph0   = sphere(vec3(-10.0, 1000.0, 0.0), vec3(0.0, 0.0, 0.8), 25.0);
 
 	// Sattelite Orange
 	sphere sph1   = sphere(vec3(sph0.C.x + (sph0.r + 250.0)*cos(VITESSE), sph0.C.y + (sph0.r + 250.0)*sin(VITESSE), sph0.C.z), vec3(0.88, 0.41, 0.0), 35.0);
@@ -489,13 +414,13 @@ void scene_SateliteOrbital(void) // Manque rotation orbitale  float(uTime) modul
 	NB_SPHERES    = 4;
 
 	// ===== Init des sources de lumiere
-	T_SRC[0] = source(vec3(200.0, 100.0, 0.0), vec3(10.0, 10.0, 10.0));
+	T_SRC[0] = source(vec3(200.0, 100.0, 0.0), vec3(7.0, 7.0, 7.0));
 
 	NB_SOURCES = 1;
 
 	// ===== Dessin
-	draw_raw_spheres(rayon);
-	//draw_shines_spheres(rayon);
+	//draw_raw_all(rayon);
+	draw_shined_all(rayon);
 }
 
 void scene_Boite_Collier(void) // Rotation du collier horaire et changement des couleurs
@@ -529,77 +454,38 @@ void scene_Boite_Collier(void) // Rotation du collier horaire et changement des 
 	NB_SOURCES = 1;
 
 	// ===== Dessin
-
-
-	for(int i=0; i<1000; i++)
-	{
-		if(i >= NB_SPHERES)
-		{
-			break;
-		}
-		draw_sphere(rayon, T_SPH[i]);
-	}
-
-	for(int i=0; i<100; i++)
-	{
-		if(i >= NB_SPHERES)
-		{
-			break;
-		}
-	shine_sphere(rayon, T_SPH[i]);
-	}
+	//draw_raw_all(rayon);
+	draw_shined_all(rayon);
 }
 
 void scene_Lueures(void)
 {
-	// ===== Couleur de la scene
-	gl_FragColor  = vec4(1.0, 1.0, 1.0, 1.0) ;
-
 	// Init des structs
-	ray    rayon  = ray(uOriVector, pixCenter, -1.0);
+	ray rayon   = ray(uOriVector, pixCenter, -1.0);
 
 	// 1 plan pour le sol
-	plan   plan1  = plan(vec3(0.0, 0.0, -10.0), vec3(0.10, 0.0, 0.75), vec3(0.0, 0.5, 0.0));
+	T_PLANS[0]  = plan(vec3(0.0, 0.0, -30.0), vec3(0.0, 0.0, 1.0), vec3(0.2, 0.2, 0.2));
+	NB_PLANS    = 1;
 
-	// 1 sphere
-	sphere sph0   = sphere(vec3(-10.0, 300.0, 0.0), vec3(0.9, 0.9, 0.9), 30.0);
-
-	sphere sph1   = sphere(vec3(sph0.C.x + (sph0.r + 50.0)*cos(VITESSE), sph0.C.y + (sph0.r + 50.0)*sin(VITESSE), sph0.C.z), vec3(10.0, 0.0, 0.0), 1.0);
-	sphere sph2   = sphere(vec3(sph0.C.x - (sph0.r + 50.0)*cos(VITESSE), sph0.C.y + (sph0.r + 50.0)*sin(VITESSE), sph0.C.z), vec3(0.0, 0.0, 10.0), 1.0);
-	T_SPH[0]      = sph0;
-	T_SPH[1]      = sph1;
-	T_SPH[2]      = sph2;
-
-	NB_SPHERES = 3;
+	// 1 sphere cenrale + 2 tournantes
+	sphere sph0 = sphere(vec3(0.0, 200.0, 0.0), vec3(0.9, 0.9, 0.9), 30.0);
+	sphere sph1 = sphere(vec3(sph0.C.x + (sph0.r + 50.0)*cos(VITESSE), sph0.C.y + (sph0.r + 50.0)*sin(VITESSE), sph0.C.z), vec3(10.0, 0.0, 0.0), 1.0);
+	sphere sph2 = sphere(vec3(sph0.C.x - (sph0.r + 50.0)*cos(VITESSE), sph0.C.y + (sph0.r + 50.0)*sin(VITESSE), sph0.C.z), vec3(0.0, 0.0, 10.0), 1.0);
+	T_SPH[0]    = sph0;
+	T_SPH[1]    = sph1;
+	T_SPH[2]    = sph2;
+	NB_SPHERES  = 3;
 
 	// ===== Init des sources de lumiere
-	T_SRC[0] = source(vec3(200.0, 100.0, 0.0), vec3(3.0, 3.0, 3.0));
-	T_SRC[1] = source(vec3(sph0.C.x + (sph0.r + 50.0)*cos(VITESSE), sph0.C.y + (sph0.r + 50.0)*sin(VITESSE), sph0.C.z), vec3(10.0, 0.0, 0.0));
-	T_SRC[2] = source(vec3(sph0.C.x - (sph0.r + 50.0)*cos(VITESSE), sph0.C.y + (sph0.r + 50.0)*sin(VITESSE), sph0.C.z), vec3(0.0, 0.0, 10.0));
-	NB_SOURCES = 3;
+	T_SRC[0]    = source(vec3(200.0, 100.0, 0.0), vec3(3.0, 3.0, 3.0));
+	T_SRC[1]    = source(vec3(sph0.C.x + (sph0.r + 50.0)*cos(VITESSE), sph0.C.y + (sph0.r + 50.0)*sin(VITESSE), sph0.C.z), vec3(10.0, 0.0, 0.0));
+	T_SRC[2]    = source(vec3(sph0.C.x - (sph0.r + 50.0)*cos(VITESSE), sph0.C.y + (sph0.r + 50.0)*sin(VITESSE), sph0.C.z), vec3(0.0, 0.0, 10.0));
+	NB_SOURCES  = 3;
 
 
 	// ===== Dessin
-
-	draw_plan(rayon, plan1);
-
-	for(int i=0; i<1000; i++)
-	{
-		if(i >= NB_SPHERES)
-		{
-			break;
-		}
-		draw_sphere(rayon, T_SPH[i]);
-	}
-
-	for(int i=0; i<100; i++)
-	{
-		if(i >= NB_SPHERES)
-		{
-			break;
-		}
-	shine_sphere(rayon, T_SPH[i]);
-	}
+	//draw_raw_all(rayon);
+	draw_shined_all(rayon);
 }
 
 
@@ -611,4 +497,5 @@ void main(void)
 	scene_SateliteOrbital();
 	//scene_Boite_Collier();
 	//scene_Lueures();
+	//scene_atome();
 }
