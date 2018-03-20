9bafa158e602051370673a6f089a9dfb3c8d9a5d
Thanh <tlutru@gmail.com>
2018-03-21 00:57:35 +0100

    Renommage dossiers traitement; Add growing region

---
+++ 4ASTI/traitement_image/6td_croissance_region/growing_regions.cpp
@@ -0,0 +1,143 @@
+/* ===========================
+			INCLUDES
+   =========================== */
+
+#include "growing_regions.h"
+
+/* ===========================
+		   NAMESPACES
+   =========================== */
+
+using namespace cv;
+using namespace std;
+
+/* ===========================
+			  MAIN 
+   =========================== */
+
+int main(int argc, char const *argv[])
+{
+	if(argc < 5)
+    {
+        cout << "Usage: ./app fp_image x y threshold" << endl
+             << "fp_image:  Filepath to source image" << endl
+             << "x:         X position of the starting pixel" << endl
+             << "y:         Y position of the starting pixel" << endl
+             << "threshold: Value of minimal threshold" << endl;
+        return 0;
+    }
+
+    Mat image = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+    if(!image.data)
+    {
+        printf("No image data \n");
+        return -1;
+    }
+
+    // === Init des variables
+    int   x     = stoi(argv[2]);
+    int   y     = stoi(argv[3]);
+
+    if(x > image.size().height || y > image.size().width)
+    {
+    	cout << "Position du pixel invalide" << endl
+    	     << "data (x,y)   == (" << x << "," << y << ")" << endl
+    	     << "taille image == " << image.size().height << "x" << image.size().width << endl;
+             return -1;
+    }
+
+    Mat   dst       = Mat(image.size().height, image.size().width, CV_8UC1, Scalar(0));
+    float seed      = (float)image.at<uchar>(x,y);
+    float threshold = stof(argv[4]);
+    int   n         = 1;
+
+    // === Recursivite
+    dst.at<uchar>(x,y) = 255; // situation initiale
+    growing(image, dst, Point(x,y), seed, n, threshold);
+
+    // === Print
+    afficheImage(image, "Source");
+    afficheImage(dst, "Growing region");
+    waitKey(0);
+
+	return 0;
+}
+
+/* ===========================
+			FONCTIONS
+   =========================== */
+
+// =========================== PRINT
+void afficheImage(const Mat& src, String name)
+{
+    namedWindow(name, WINDOW_AUTOSIZE );
+    imshow(name, src);
+}
+
+
+// =========================== GROWING-REGION
+void growing(const Mat& src, Mat& dst, Point current, float& sum, int& n, float& threshold)
+{
+	float mean  = sum / (float)n;
+    int   sizeX = src.size().height;
+    int   sizeY = src.size().width;
+    int   x     = current.x;
+    int   y     = current.y;
+    float value = 0;
+
+    dst.at<uchar>(x, y) = 255; // On colorie celui sur lequel on est
+    for(int voisin=kGauche; voisin<kTousLesVoisins; voisin++)
+    {
+        if(isValid(dst, Point(x+kMvtX[voisin], y+kMvtY[voisin]), sizeX, sizeY))
+        {
+            if(inArea(src, Point(x+kMvtX[voisin], y+kMvtY[voisin]), mean, threshold))
+            {
+                value = (float)src.at<uchar>(x+kMvtX[voisin], y+kMvtY[voisin]);
+                update(value, sum, n, threshold);
+
+                growing(src, dst, Point(x+kMvtX[voisin], y+kMvtY[voisin]), sum, n, threshold);
+            }
+            else dst.at<uchar>(x+kMvtX[voisin], y+kMvtY[voisin]) = 128; // rejected
+        }
+    }
+}
+
+void update(float value, float& sum, int& n, float& threshold)
+{
+    sum += value;
+    n++;
+    // threshold dynamique ...
+}
+
+// =========================== BOOLEAN
+
+bool inImage(Point pix, int sizeX, int sizeY)
+{
+    if(pix.x < 0     || pix.y < 0)     return false;
+    if(pix.x > sizeX || pix.y > sizeY) return false;
+    return true;
+}
+
+bool inArea(const Mat& src, Point voisin, float mean, float threshold)
+{
+    float res = abs((float)src.at<uchar>(voisin.x, voisin.y) - mean);
+
+    return (res < threshold);
+}
+
+bool isMarked(Mat& dst, Point pix)
+{
+    int value = (int)dst.at<uchar>(pix.x, pix.y);
+
+    if(value == 255) return true;
+    if(value == 128) return true;
+
+    return false;
+}
+
+bool isValid(Mat& dst, Point pix, int sizeX, int sizeY)
+{
+    if(inImage(pix, sizeX, sizeY)) 
+        return !isMarked(dst, pix);
+    return false;
+}
---
+++ 4ASTI/traitement_image/6td_croissance_region/growing_regions.h
@@ -0,0 +1,38 @@
+#ifndef _GROWING_REGIONS_H_
+#define _GROWING_REGIONS_H_
+
+#include <stdio.h>
+#include <highgui.h>
+#include <opencv2/opencv.hpp>
+
+/* ===========================
+            CONSTANTES
+   =========================== */
+const int kGauche         = 0;
+const int kHaut           = 1;
+const int kDroite         = 2;
+const int kBas            = 3;
+const int kTousLesVoisins = 4;
+
+const int kMvtX[4] = { 0, -1, 0, 1};
+const int kMvtY[4] = {-1,  0, 1, 0};
+
+
+/* ===========================
+            PROTOTYPES
+   =========================== */
+
+// =========================== PRINT
+void afficheImage(const cv::Mat& src, cv::String name);
+
+// =========================== GROWING-REGION
+void growing(const cv::Mat& src, cv::Mat& dst, cv::Point current, float& sum, int& n, float& threshold);
+void update(float value, float& sum, int& n, float& threshold);
+
+// =========================== BOOLEAN
+bool inImage(cv::Point pix, int sizeX, int sizeY);
+bool inArea(const cv::Mat& src, cv::Point voisin, float mean, float threshold);
+bool isMarked(cv::Mat& dst, cv::Point pix);
+bool isValid(cv::Mat& dst, cv::Point pix, int sizeX, int sizeY);
+
+#endif
