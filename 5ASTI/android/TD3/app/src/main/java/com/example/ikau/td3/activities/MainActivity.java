8393889914f0148c7422aaccb3c87ffd77797694
Ikau <tlutru@gmail.com>
2018-12-12 18:49:24 +0100

    :sparkles: New package structure :sparkles: Callback first button

---
+++ 5ASTI/android/TD3/app/src/main/java/com/example/ikau/td3/activities/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.ikau.td3.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.ikau.td3.R;
+import com.example.ikau.td3.enums.ActionsEnum;
+import com.example.ikau.td3.tasks.AsyncFlickrTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void onClickButtonResults(View v)
+    {
+        String urlString = "https://www.flickr.com/services/feeds/photos_public.gne?tags=cats&format=json";
+        new AsyncFlickrTask(this).execute(urlString, ActionsEnum.PLAIN_JSON);
+    }
+}
