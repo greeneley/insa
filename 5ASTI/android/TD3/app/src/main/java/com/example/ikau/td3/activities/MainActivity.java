cd438d6281a4a0b9d39c4a4fc78b0e6a45f5d1a4
Ikau <tlutru@gmail.com>
2018-12-13 10:43:02 +0100

    :sparkles: Dynamic fragment

--- 5ASTI/android/TD3/app/src/main/java/com/example/ikau/td3/activities/MainActivity.java
+++ 5ASTI/android/TD3/app/src/main/java/com/example/ikau/td3/activities/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.ikau.td3.activities;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.ikau.td3.R;
 import com.example.ikau.td3.enums.ActionsEnum;
+import com.example.ikau.td3.fragments.PlaceholderFragment;
+import com.example.ikau.td3.fragments.ProgressSpinnerFragment;
 import com.example.ikau.td3.tasks.AsyncFlickrTask;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set default fragment
+        this.setMainFragment(new PlaceholderFragment());
     }
 
     protected void onClickButtonResults(View v)
@@ -35,5 +46,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onClickButtonAdvanced(View v)
     {
         // TODO
+    }
+
+    private void setMainFragment(Fragment fragment)
+    {
+        getSupportFragmentManager()
+            .beginTransaction()
+            .replace(R.id.layoutFragment, fragment)
+            .commit();
+    }
+
+    public void showProgressBar()
+    {
+        getSupportFragmentManager()
+            .beginTransaction()
+            .replace(R.id.layoutFragment, new ProgressSpinnerFragment())
+            .commit();
     }
 }
