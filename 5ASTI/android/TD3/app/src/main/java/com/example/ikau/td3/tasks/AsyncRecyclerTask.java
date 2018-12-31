0e20e818b42c2bc17d279e6830e6c47db6ba2a90
Ikau <tlutru@gmail.com>
2018-12-31 15:20:53 +0100

    :sparkles: Empty class for RecyclerView implementation

---
+++ 5ASTI/android/TD3/app/src/main/java/com/example/ikau/td3/tasks/AsyncRecyclerTask.java
@@ -0,0 +1,8 @@
+package com.example.ikau.td3.tasks;
+
+public class AsyncRecyclerTask extends AbstractAsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
